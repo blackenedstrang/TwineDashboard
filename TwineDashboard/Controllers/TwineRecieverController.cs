a525644a5cae5ddc5da0d717dba569b3fb8b8d79
blackened <blackened>
2012-12-27 00:16:36 +0200

    Nothing interesting...establishing basic structure.

--- TwineDashboard/Controllers/TwineRecieverController.cs
+++ TwineDashboard/Controllers/TwineRecieverController.cs
@@ -1,25 +1,17 @@
 ﻿using System.Collections.Generic;
 using System.Collections.Specialized;
-using System.Web;
 using System.Web.Http;
 
 namespace TwineDashboard.Controllers
 {
 	public class TwineRecieverController : ApiController
 	{
-		//TODO: use POST instead when Twine supports that
+		//TODO: use POST when Twine supports that
 		//TODO: try async/await
-		public void Get()
+		[HttpGet]
+		public string HighTemperature(string temperature)
 		{
-			//http://localhost:5935/api/TwineReciever/?temperature=25&orientation=top
-			var nvc = HttpUtility.ParseQueryString(Request.RequestUri.Query);
-			var temperature = nvc["temperature"];
-			var orientation = nvc["orientation"];
+			return temperature += "";
 		}
-
-		// POST api/<controller>
-		//public void Post([FromBody]string value)
-		//{
-		//}
 	}
 }
